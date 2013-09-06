f076f532a30bf15a0430daeee4d99cb9fe1074c5
Mila Frerichs <mila.frerichs@gmail.com>
2013-09-06 16:39:20 +0200

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Votenow::Application.config.secret_key_base = 'bd3faaba1275953a9e440d6244c542cf03aecdf9c9c4b32803b320fbbeaeb7f75ccbd93bdb9581b3c29d7192f7536d146959de9ed61b3c3af1cafccf75123b0c'
