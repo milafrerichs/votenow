2db0c344d65dc182a3caee2d24bfb4e93f6ffd00
Oliver Noack <oliver.noack@zweitag.de>
2013-09-06 19:34:28 +0200

    Add index links and use gon in controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,6 +8,15 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
+
+    gon.data = [
+      {name: 'Category Name', score: 2.3},
+      {name: 'lala', score: 4.3},
+      {name: 'lalal2', score: 2.6},
+      {name: 'lalala3', score: 1.3},
+      {name: 'clalalal3', score: 2.3},
+      {name: 'clalal3', score: 2.3}
+    ]
   end
 
   def vote
--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,8 @@ class Application < ActiveRecord::Base
   def name
     github_link.split('/').last.capitalize
   end
+
+  def to_s
+    name
+  end
 end
