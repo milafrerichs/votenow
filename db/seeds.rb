9753f727430eb1f7c37023e5b6c1032bb6e5879b
Oliver Noack <oliver.noack@zweitag.de>
2013-09-06 17:24:18 +0200

    Add application seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Application.create(github_link: 'http://github.com/tho85/lunchpad')
+Application.create(github_link: 'http://github.com/tilt/socially_awkward')
+Application.create(github_link: 'http://github.com/NiDi/suckerfish')
+Application.create(github_link: 'http://github.com/milafrerichs/votenow')
+
